b69d33840d66123b0fff748517dd1c862178da42
user-faiz <email2faizm@gmail.com>
2024-02-14 00:39:54 +0530

    config data model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,9 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods, foreign_key: 'food_id'
+  has_many :recipes, through: :recipe_foods
+
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :measurement_unit, presence: true
 end
