b0e8f8f6a33527b6088e7db44b8bcb4a6fbfa553
user-faiz <email2faizm@gmail.com>
2024-02-15 12:41:23 +0530

    update schema, food.rb and views

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,4 +6,6 @@ class Food < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :name, presence: true, length: { maximum: 250 }
   validates :measurement_unit, presence: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
 end
