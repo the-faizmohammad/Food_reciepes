9b878b7a119e73947356e19eeca930b139509c95
user-faiz <email2faizm@gmail.com>
2024-02-15 21:30:24 +0530

    define test for models

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do
+    RecipeFood.delete_all
+    Food.delete_all
+    Recipe.delete_all
+    User.delete_all
+    @user = User.create(name: 'User', surname: 'Spec', email: 'testing@gmail.com', password: 'testing')
+    @r1 = Recipe.create(user: @user, name: 'Recipe one', preparation_time: 1, cooking_time: 2,
+                        description: 'Recipe one description', public: true)
+    @f1 = Food.create(user: @user, name: 'Food item', measurement_unit: 'kg', price: 2, quantity: 5)
+    @rf1 = RecipeFood.create(recipe_name: @r1, food_name: @f1, quantity: 2)
+  end
+
+  context 'checks quantity validation' do
+    it 'Food should have quantity' do
+      rf2 = RecipeFood.create(recipe_name: @r1, food_name: @f1)
+      expect(rf2).not_to be_valid
+    end
+
+    it 'Food should have quantity' do
+      rf2 = RecipeFood.create(food_name: @f1, quantity: 1)
+      expect(rf2).not_to be_valid
+    end
+
+    it 'Food should have quantity' do
+      rf2 = RecipeFood.create(recipe_name: @r1, quantity: 1)
+      expect(rf2).not_to be_valid
+    end
+  end
+
+  context '#calculate_value' do
+    it 'returns value based on quantity and food price' do
+      expect(@rf1.calculate_value).to eq(4)
+    end
+  end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do
+    RecipeFood.delete_all
+    Food.delete_all
+    Recipe.delete_all
+    User.delete_all
+    @user = User.create(name: 'User', surname: 'Spec', email: 'testing@gmail.com', password: 'testing')
+    @r1 = Recipe.create(user: @user, name: 'Recipe one', preparation_time: 1, cooking_time: 2,
+                        description: 'Recipe one description', public: true)
+    @r2 = Recipe.create(user: @user, name: 'Recipe two', preparation_time: 2, cooking_time: 3,
+                        description: 'Recipe two description', public: false)
+    @r3 = Recipe.create(user: @user, name: 'Recipe three', preparation_time: 1, cooking_time: 1,
+                        description: 'Recipe three description', public: true)
+  end
+
+  context '#create instance' do
+    it 'Recipe should be valid' do
+      expect(@r1).to be_valid
+      expect(@r2).to be_valid
+    end
+
+    it 'User association should be present' do
+      r = Recipe.create(name: 'Recipe one', preparation_time: 1, cooking_time: 2,
+                        description: 'Recipe one description', public: true)
+      expect(r).not_to be_valid
+    end
+  end
+
+  context 'Check name validation' do
+    it 'name should be present' do
+      @r4 = Recipe.create(user: @user, preparation_time: 1, cooking_time: 2,
+                          description: 'Recipe one description', public: true)
+      expect(@r4).not_to be_valid
+    end
+
+    it 'name should be below 250 chars' do
+      @r3.name = 'A' * 252
+      expect(@r3).not_to be_valid
+    end
+  end
+
+  context 'Check description, preparation_time, cooking_time validation' do
+    it 'description should be below 2000 words' do
+      r5 = Recipe.create(user: @user, name: 'Recipe 5', preparation_time: 1, cooking_time: 2,
+                         description: 'Recipe one description' * 2001, public: true)
+      expect(r5).not_to be_valid
+    end
+
+    it 'preparation_time should be a interger' do
+      r6 = Recipe.create(user: @user, name: 'Recipe 6', preparation_time: -1, cooking_time: 2,
+                         description: 'Recipe one description', public: true)
+      expect(r6).not_to be_valid
+    end
+
+    it 'cooking_time should be a interger' do
+      r7 = Recipe.create(user: @user, name: 'Recipe 6', preparation_time: 1, cooking_time: -1,
+                         description: 'Recipe one description', public: true)
+      expect(r7).not_to be_valid
+    end
+  end
 end
